4b40d28feee90803066e7a4f74d0455d8ee529d5
YuYu1015 <whes1015@gmail.com>
2025-02-15 20:45:07 +0800

    fix: build crash

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+const basePath = isProduction ? `/weather-dashboard` : "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  basePath: basePath,
+  assetPrefix: basePath,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
