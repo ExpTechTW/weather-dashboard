e27282a6973835c6da25bc034a215e054d2e2daa
PiscesXD <rclu2881@gmail.com>
2025-05-05 02:06:45 +0800

    feat: colors

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -14,3 +14,26 @@ moment.locale('zh-tw');
 export const time = (t: moment.MomentInput) => moment(t).format('HH:mm:ss');
 
 export const date = (t: moment.MomentInput) => moment(t).format('yyyy年MM月DD日 dddd');
+
+export function getColor(intensity: number): string {
+  switch (intensity) {
+    case 0:
+      return '#FFFFFF';
+    case 1:
+      return '#D4F2D2';
+    case 2:
+      return '#7BEA7B';
+    case 3:
+      return '#FFFF00';
+    case 4:
+      return '#FFA500';
+    case 5:
+      return '#FF0000';
+    case 6:
+      return '#800000';
+    case 7:
+      return '#FF00FF';
+    default:
+      return '#FFFFFF';
+  }
+}
