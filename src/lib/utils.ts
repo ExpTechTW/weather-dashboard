c97c885fdd9fbba3ac48221082ae892eefe0ac0e
PiscesXD <rclu2881@gmail.com>
2025-05-05 20:39:04 +0800

    feat: getIntensityText

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -33,7 +33,38 @@ export function getIntensityColor(intensity: number): string {
       return '#fc5235';
     case 7:
       return '#b720e9';
+    case 8:
+      return '#800080';
+    case 9:
+      return '#ff0000';
     default:
       return '#79E5FD';
+  }
+}
+
+export function getIntensityText(intensity: number): string {
+  switch (intensity) {
+    case 0:
+      return '0';
+    case 1:
+      return '1';
+    case 2:
+      return '2';
+    case 3:
+      return '3';
+    case 4:
+      return '4';
+    case 5:
+      return '5弱';
+    case 6:
+      return '5強';
+    case 7:
+      return '6弱';
+    case 8:
+      return '6強';
+    case 9:
+      return '7';
+    default:
+      return '0';
   }
 }
