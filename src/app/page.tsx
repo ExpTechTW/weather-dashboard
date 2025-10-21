ca5ead288190f9d04a7424ea28453c08553f7b1e
PiscesXD <rclu2881@gmail.com>
2025-10-21 17:06:50 +0800

    feat: loading page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
+'use client';
 
+import { LoaderCircle } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/dashboard');
+  }, []);
+
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <div className="flex flex-col items-center gap-4">
+        <LoaderCircle className="animate-spin" size={32} />
+        <div className="text-2xl font-bold">
+          載入中
+        </div>
+      </div>
+    </div>
+  );
 }
